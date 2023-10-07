da1fc2a24bfcbb5b55e5f7a83d749e83f446c2ed
ilkerteymur <ilkerteymuroglu@gmail.com>
2023-10-07 16:45:46 +0300

    deployed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# weatherApp
+# Weather-App
+
+- Received data from api with fetch.
+
+- Added icon according to weather conditions.
+
+- It's a global weather project.
+
+### 'Deploy'
+
+https://splendorous-fairy-8e087d.netlify.app/
+
+### 'Screen'
+
+![](screen.gif)
--- script.js
+++ script.js
@@ -1,38 +1,79 @@
-const url = 'https://api.openweathermap.org/data/2.5/';
-const key = '7994916e07a2aaf37f9f5fa8d5cc0570';
+// apı ın url ı key ıcınde degıskende saklanır
+const url = "https://api.openweathermap.org/data/2.5/";
+const key = "7994916e07a2aaf37f9f5fa8d5cc0570";
 
-const searchBar = document.getElementById('searchBar');
-searchBar.addEventListener('keypress', setQuery);
+// !html den gelenler
+const box = document.querySelector(".weather-box");
+const searchBar = document.getElementById("searchBar");
+
+// klavye ıle olay ızleyıcısı
+searchBar.addEventListener("keypress", setQuery);
 
 function setQuery(e) {
-  if (e.key === 'Enter') {
+  if (e.key === "Enter") {
     getResult(searchBar.value);
   }
 }
-
+// kullanıcı sehır ısmı gırdıgınde apı a ıstek atar
 function getResult(cityName) {
-  let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`   
+  let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
   fetch(query)
-  .then(weather => {
-    return weather.json()
-
-  })
-  .then(displayResult)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResult);
 }
 
-const displayResult = (result) =>{
-    let city = document.querySelector('.city')
-    city.innerText = `${result.name},${result.sys.country}`
+// sehrin sonucunu ogrenme ve ekrana basma
+const displayResult = (result) => {
+  let city = document.querySelector(".city");
+  city.innerText = `${result.name},${result.sys.country}`;
 
-    let temp = document.querySelector('.temp')
-    temp.innerText = `${Math.round(result.main.temp)}°C`
+  let temp = document.querySelector(".temp");
+  temp.innerText = `${Math.round(result.main.temp)}°C`;
 
-    let desc = document.querySelector('.desc')
-    desc.innerText = result.weather[0].description
+  let desc = document.querySelector(".desc");
+  desc.innerText = result.weather[0].description;
 
-    let minmax = document.querySelector('.minmax')
-    minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
-     
+  let minmax = document.querySelector(".minmax");
+  minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(
+    result.main.temp_max
+  )}°C`;
 
-}
+  const image = document.createElement("img");
+  console.dir(result);
+  // switch case ile hava durumuna göre resim ekledim
+  switch (result.weather[0].description) {
+    case "açık":
+      image.src = "günes.png";
+      break;
+    case "parçalı bulutlu":
+      image.src = "kapali.png";
+      break;
+    case "az bulutlu":
+      image.src = "kapalii.png";
+      break;
+    case "parçalı az bulutlu":
+      image.src = "kapalii.png";
+      break;
 
+    case "kapalı":
+      image.src = "kapalii.png";
+      break;
+    case "kar yağışlı":
+      image.src = "karli.png";
+      break;
+    case "hafif yağmur":
+      image.src = "yagmur.png";
+      break;
+    case "yagmurlu":
+      image.src = "yagmur.png";
+      break;
+    // Diğer hava durumları için eklemeler yapabilirsiniz
+    default:
+      image.src = ""; // Varsayılan durumda resmi kaldırabilirsiniz
+      break;
+  }
+  box.innerHTML = "";
+  box.appendChild(image);
+};
